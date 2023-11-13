e136cc95612485348f14ffaa72a6a0245007ff75
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-11-13 17:28:11 +0100

    seeds are OK

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,46 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Destroy messages, users and channels (in that order)
+puts 'Destroy messages, users and channels :'
+Message.destroy_all
+User.destroy_all
+Channel.destroy_all
+puts 'all gone!'
+puts '------------'
+
+# Create some channels (general, paris, react for instance)
+puts 'Creating channels'
+['general', 'paris', 'react'].each do |channel|
+  Channel.create(name: channel)
+end
+puts '3 channels created!'
+puts '------------'
+
+# Create some users (needs email / password)
+puts 'Creating users'
+['cyrielle@gmail.com', 'paul@gmail.com', 'mike@gmail.com'].each do |user|
+  User.create(email: user, password: "123456")
+end
+puts '3 users created!'
+puts '------------'
+
+# Create some messages in several channels.
+puts 'Creating messages'
+channels = Channel.all
+users = User.all
+users.each do |user|
+  channels.each do |channel|
+    new_message = Message.new(user: user, channel: channel)
+    new_message.content = "hello from #{new_message.user.email[0..-9]} in #{new_message.channel.name} !!"
+    new_message.save
+  end
+end
+puts 'Some messages created!'
+puts '------------'
+
+if User.all.size > 2 && Channel.all.size > 2 && Message.all.size > 2
+  puts 'Seeds are successfully done !'
+  puts '------------'
+end
